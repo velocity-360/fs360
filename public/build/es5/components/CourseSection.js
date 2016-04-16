a9acd3a163da698873861b2557a9308bc624066f
fullstack360 <info@fullstack360.com>
2016-04-16 16:40:57 -0400

    removed commented out code

--- public/build/es5/components/CourseSection.js
+++ public/build/es5/components/CourseSection.js
@@ -27,14 +27,6 @@ var CourseSection = (function (Component) {
 	_prototypeProperties(CourseSection, null, {
 		render: {
 			value: function render() {
-				console.log("ACCOUNT TYPE: " + this.props.accountType);
-
-				// var videoThumb = null;
-				// if (this.props.course.type == 'online'){
-				// 	videoThumb = <div className={'wistia_embed wistia_async_'+this.props.unit.wistia+' videoFoam=true'} style={{height:200, width:356, marginTop:12}}>&nbsp;</div>
-				// }
-
-
 				var videoThumb = null;
 				if (this.props.course.type == "online") {
 					if (this.props.unit.index < 1) {
@@ -78,21 +70,6 @@ var CourseSection = (function (Component) {
 						);
 					}
 				}
-
-				// if (this.props.course.type == 'online'){
-				// if (this.props.unit.index < 2){ // always show first video
-				// 	videoThumb = <span className={'wistia_embed wistia_async_'+this.props.section.wistia+' popover=true popoverAnimateThumbnail=true'} style={{display:'inline-block',height:168,width:300,marginTop:24}}>&nbsp;</span>;
-				// }
-				// else if (this.props.accountType == 'premium'){
-				// 	videoThumb = <span className={'wistia_embed wistia_async_'+this.props.section.wistia+' popover=true popoverAnimateThumbnail=true'} style={{display:'inline-block',height:168,width:300,marginTop:24}}>&nbsp;</span>;
-				// }
-				// else if (this.props.accountType == 'basic' || this.props.accountType == ''){
-				// 	videoThumb = <div style={{border:'1px solid #ddd', padding:12, background:'#fff', marginTop:12, marginBottom:12}}>To view this video, please <a style={{color:'red'}} onClick={this.subscribeAction} href="#">upgrade</a> your account to Premium</div>
-				// }
-				// else if (this.props.accountType == 'none'){ // not logged in
-				// 	videoThumb = <div style={{border:'1px solid #ddd', padding:12, background:'#fff', marginTop:12, marginBottom:12}}>Please log in or <a style={{color:'red'}} href="/premium">register</a> to view this video.</div>
-				// }
-				// }
 
 				return React.createElement(
 					"div",
