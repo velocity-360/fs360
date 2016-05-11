5c0de6e1fb1a4e9c492ca682dae0e7d496933a4c
fullstack360 <info@fullstack360.com>
2016-05-11 14:51:07 -0400

    add topic and tags to Sample

--- models/Sample.js
+++ models/Sample.js
@@ -3,12 +3,14 @@ var mongoose = require('mongoose')
 var SampleSchema = new mongoose.Schema({
 	profile: {type:String, trim:true, default:''},
 	title: {type:String, trim:true, default:''},
+	topic: {type:String, trim:true, lowercase:true, default:'ios'}, // ios, node, react, etc
 	image: {type:String, trim:true, default:''},
 	url: {type:String, trim:true, default:''},
 	isPublic: {type:String, trim:true, default:'yes'},
 	video: {type:String, trim:true, default:''},
 	description: {type:String, default:''},
 	slug: {type:String, trim:true, lowercase:true, default:''},
+	tags: {type:Array, default:[]},
 	timestamp: {type:Date, default: Date.now}
 })
 
@@ -16,12 +18,14 @@ SampleSchema.methods.summary = function() {
 	var summary = {
 		'profile':this.profileitle,
 		'title':this.title,
+		'topic':this.topic,
 		'image':this.image,
 		'url':this.url,
 		'isPublic':this.isPublic,
 		'video':this.video,
 		'description':this.description,
 		'slug':this.slug,
+		'tags':this.tags,
 		'timestamp':this.timestamp,
 		'id':this._id
 	};
