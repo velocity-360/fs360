0d4cd84bd887f32e20060890673198aa8be248ea
fullstack360 <info@fullstack360.com>
2016-06-17 15:50:38 -0400

    replace semi-colons

--- managers/Helpers.js
+++ managers/Helpers.js
@@ -14,22 +14,22 @@ module.exports = {
 	    if (string == null)
 	        return;
 	    
-	    var parts = string.split(' ');
-	    var capitalizedString = '';
+	    var parts = string.split(' ')
+	    var capitalizedString = ''
 	    
 	    for (var i=0; i<parts.length; i++){
-	        var s = parts[i];
+	        var s = parts[i]
 	        if (s.length <= 1){
-	            capitalizedString = capitalizedString+' '+s.toUpperCase();
-	            continue;
+	            capitalizedString = capitalizedString+' '+s.toUpperCase()
+	            continue
 	        }
 	        
-	        s = s.charAt(0).toUpperCase() + s.slice(1);
-	        capitalizedString = capitalizedString+' '+s;
+	        s = s.charAt(0).toUpperCase() + s.slice(1)
+	        capitalizedString = capitalizedString+' '+s
 	    }
 	    
-	    capitalizedString = capitalizedString.trim();
-	    return capitalizedString;
+	    capitalizedString = capitalizedString.trim()
+	    return capitalizedString
 	},
 
 	slugString: function(string){
@@ -38,6 +38,8 @@ module.exports = {
 		var slug = ''
 		for (var i=0; i<parts.length; i++){
 			var word = parts[i]
+			word = word.replace('?', '')
+			word = word.replace(':', '')
 			slug += word.toLowerCase()
 			if (i != parts.length-1)
 				slug += '-'
