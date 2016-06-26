db0ecd29f83635c632ec46cd602a23da3d2b715c
fullstack360 <info@fullstack360.com>
2016-06-26 14:18:32 -0400

    corrected tests

--- public/app/tests/Nav.spec.js
+++ public/app/tests/Nav.spec.js
@@ -6,10 +6,11 @@ import testStore from '../tests/testStore'
 
 describe('Nav', () => {
   it('shows login link', () => {
-    const element = mount(<Nav store={testStore} />)
+    const element = mount(<Nav store={store} />)
 //    console.log(element.html())
 
-    expect(element.find('.user')).to.have.length(1)
+    expect(element.find('.user')).to.have.length(0)
+    expect(element.find('.login')).to.have.length(1)
   })
 
   it('shows current user name', () => {
