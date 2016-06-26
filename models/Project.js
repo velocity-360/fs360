71d0739cc895e006dcca57908b342d4ccfa78a39
fullstack360 <info@fullstack360.com>
2016-06-26 14:40:58 -0400

    misc

--- models/Project.js
+++ models/Project.js
@@ -1,18 +1,19 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 
 var ProjectSchema = new mongoose.Schema({
 	title: {type:String, trim:true, default: ''},
 	description: {type:String, trim:true, default: ''},
-	image: {type:String, trim:true, default: 'tHyPScSk'}, // blue logo
-	images: {type: Array, default:[]},
 	slug: {type:String, lowercase:true, trim:true, default:''},
 	repo: {type:String, lowercase:true, trim:true, default:''},
 	link: {type:String, trim:true, lowercase:true, default:''}, // app store, website, etc
 	profile: {type:mongoose.Schema.Types.Mixed, default:{}},
+	image: {type:String, trim:true, default: 'tHyPScSk'}, // blue logo
+	images: {type: Array, default:[]},
 	tags: {type: Array, default:[]}, // tech used
+	units: {type:Array, default:[]}, // array of json objects {'unit':'unit one', 'description':'fawe awef fawef f'}
 	timestamp: {type:Date, default:Date.now},
-});
+})
 
 
 ProjectSchema.methods.summary = function() {
@@ -26,10 +27,11 @@ ProjectSchema.methods.summary = function() {
 		'repo':this.repo,
 		'link':this.link,
 		'tags':this.tags,
+		'units':this.units,
 		'timestamp':this.timestamp,
 		'id':this._id
-	};
-	return summary;
-};
+	}
+	return summary
+}
 
-module.exports = mongoose.model('ProjectSchema', ProjectSchema);
+module.exports = mongoose.model('ProjectSchema', ProjectSchema)
