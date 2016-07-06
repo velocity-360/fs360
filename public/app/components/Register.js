21eda27c1b007b81fea9f5f5b1307f65d181aa75
fullstack360 <info@fullstack360.com>
2016-07-06 01:42:50 -0400

    misc

--- public/app/components/Register.js
+++ public/app/components/Register.js
@@ -146,67 +146,65 @@ class Register extends Component {
 		}
 
 		return (
-				<section id="register" className="section pricing-section nomargin" style={{backgroundColor: '#FFF'}}>
-					<Loader options={loaderConfig} loaded={!this.state.showLoader} className="spinner" loadedClassName="loadedContent" />
-					<div className="container clearfix">
-						<h2 className="pricing-section--title center">Cant make it to our live courses?</h2>
-						<div style={{textAlign:'center'}}>
-							<p style={{fontSize:16}}>
-								Join our online service. <br />Online members 
-								have access to videos, code samples, the forum and more.
-							</p>
+			<section id="register" className="section pricing-section nomargin" style={{backgroundColor: '#FFF'}}>
+				<Loader options={loaderConfig} loaded={!this.state.showLoader} className="spinner" loadedClassName="loadedContent" />
+				<div className="container clearfix">
+					<h2 className="pricing-section--title center">Cant make it to our live courses?</h2>
+					<div style={{textAlign:'center'}}>
+						<p style={{fontSize:16}}>
+							Join our online service. <br />Online members 
+							have access to videos, code samples, the forum and more.
+						</p>
 
+					</div>
+					<div className="pricing pricing--jinpa">
+						<div className="pricing--item" style={{marginRight:24}}>
+							<h3 className="pricing--title">Basic</h3>
+							<div style={{fontSize: '1.15em'}} className="pricing--price">FREE</div>
+							<div style={{ borderTop:'1px solid #eee', marginTop:24, paddingTop:24}}>
+								<ul className="pricing--feature-list">
+									<li className="pricing--feature">Limited Video Access</li>
+									<li className="pricing--feature">Forum Access</li>
+									<li className="pricing--feature">Discounts to Live Events</li>
+								</ul>
+							</div>
+							<button onClick={this.showRegistrationForm} id="basic" className="pricing--action">Join</button>
 						</div>
-						<div className="pricing pricing--jinpa">
-							<div className="pricing--item" style={{marginRight:24}}>
-								<h3 className="pricing--title">Basic</h3>
-								<div style={{fontSize: '1.15em'}} className="pricing--price">FREE</div>
-								<div style={{ borderTop:'1px solid #eee', marginTop:24, paddingTop:24}}>
-									<ul className="pricing--feature-list">
-										<li className="pricing--feature">Limited Video Access</li>
-										<li className="pricing--feature">Forum Access</li>
-										<li className="pricing--feature">Discounts to Live Events</li>
-									</ul>
-								</div>
-								<button onClick={this.showRegistrationForm} id="basic" className="pricing--action">Join</button>
-							</div>
-							<div className="pricing--item" style={{marginRight:24, border:'1px solid #eee'}}>
-								<h3 className="pricing--title">Premium</h3>
-								<div style={{fontSize: '1.15em'}} className="pricing--price"><span className="pricing--currency">$</span>19.99/mo</div>
-								<div style={{ borderTop:'1px solid #eee', marginTop:24, paddingTop:24}}>
-									<ul className="pricing--feature-list">
-										<li className="pricing--feature">Full Video Access</li>
-										<li className="pricing--feature">Downloadable Code Samples</li>
-										<li className="pricing--feature">Customized Job Listings</li>
-										<li className="pricing--feature">Forum Access</li>
-										<li className="pricing--feature">Discounts to Live Events</li>
-									</ul>
+						<div className="pricing--item" style={{marginRight:24, border:'1px solid #eee'}}>
+							<h3 className="pricing--title">Premium</h3>
+							<div style={{fontSize: '1.15em'}} className="pricing--price"><span className="pricing--currency">$</span>19.99/mo</div>
+							<div style={{ borderTop:'1px solid #eee', marginTop:24, paddingTop:24}}>
+								<ul className="pricing--feature-list">
+									<li className="pricing--feature">Full Video Access</li>
+									<li className="pricing--feature">Downloadable Code Samples</li>
+									<li className="pricing--feature">Customized Job Listings</li>
+									<li className="pricing--feature">Forum Access</li>
+									<li className="pricing--feature">Discounts to Live Events</li>
+								</ul>
 
-								</div>
-								<button onClick={this.showRegistrationForm} id="premium" className="pricing--action">Join</button>
 							</div>
+							<button onClick={this.showRegistrationForm} id="premium" className="pricing--action">Join</button>
 						</div>
 					</div>
+				</div>
 
-			        <Modal bsSize="sm" show={this.state.showRegistration} onHide={this.hideRegistrationForm}>
-				        <Modal.Body style={{background:'#f9f9f9', padding:24, borderRadius:3}}>
-				        	<div style={{textAlign:'center'}}>
-					        	<img style={{width:96, borderRadius:48, border:'1px solid #ddd', background:'#fff', marginBottom:24}} src='/images/logo_round_green_260.png' />
-					        	<h4>{ textUtils.capitalize(this.state.membershipType) } Membership</h4>
-				        	</div>
-				        	<input onChange={this.updateUserRegistration} id="name" className="form-control" style={{marginBottom:12}} type="text" placeholder="Name" />
-				        	<input onChange={this.updateUserRegistration} id="email" className="form-control" style={{marginBottom:12}} type="text" placeholder="Email" />
-				        	<input onChange={this.updateUserRegistration} id="password" className="form-control" style={{marginBottom:12}} type="password" placeholder="Password" />
-				        	<input onChange={this.updateUserRegistration} id="promoCode" className="form-control" style={{marginBottom:12}} type="text" placeholder="Promo Code" />
-							<div style={{textAlign:'center', marginTop:24}}>
-								<a onClick={this.register} href="#" className="button button-border button-dark button-rounded button-large noleftmargin">Join</a>
-							</div>
-				        </Modal.Body>
-			        </Modal>
+		        <Modal bsSize="sm" show={this.state.showRegistration} onHide={this.hideRegistrationForm}>
+			        <Modal.Body style={{background:'#f9f9f9', padding:24, borderRadius:3}}>
+			        	<div style={{textAlign:'center'}}>
+				        	<img style={{width:96, borderRadius:48, border:'1px solid #ddd', background:'#fff', marginBottom:24}} src='/images/logo_round_green_260.png' />
+				        	<h4>{ textUtils.capitalize(this.state.membershipType) } Membership</h4>
+			        	</div>
+			        	<input onChange={this.updateUserRegistration} id="name" className="form-control" style={{marginBottom:12}} type="text" placeholder="Name" />
+			        	<input onChange={this.updateUserRegistration} id="email" className="form-control" style={{marginBottom:12}} type="text" placeholder="Email" />
+			        	<input onChange={this.updateUserRegistration} id="password" className="form-control" style={{marginBottom:12}} type="password" placeholder="Password" />
+			        	<input onChange={this.updateUserRegistration} id="promoCode" className="form-control" style={{marginBottom:12}} type="text" placeholder="Promo Code" />
+						<div style={{textAlign:'center', marginTop:24}}>
+							<a onClick={this.register} href="#" className="button button-border button-dark button-rounded button-large noleftmargin">Join</a>
+						</div>
+			        </Modal.Body>
+		        </Modal>
 
-				</section>	
-
-
+			</section>	
 		)
 	}
 }
