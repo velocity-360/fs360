f17990c9b564bb752c71041d3a98fb4db7161917
fullstack360 <info@fullstack360.com>
2016-07-10 22:25:14 -0400

    added wistia prop to posts

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,7 @@ var PostSchema = new mongoose.Schema({
 	text: {type:String, trim:true, default: ''},
 	title: {type:String, trim:true, default: ''},
 	image: {type:String, trim:true, default: 'tHyPScSk'}, // blue logo
+	wistia: {type:String, trim:true, default: ''},
 	slug: {type:String, lowercase:true, trim:true, default:''},
 	link: {type:String, trim:true, lowercase:true, default:''},
 	profile: {type:mongoose.Schema.Types.Mixed, default:{}},
@@ -19,6 +20,7 @@ PostSchema.methods.summary = function() {
 	var summary = {
 		'title':this.title,
 		'image':this.image,
+		'wistia':this.wistia,
 		'profile':this.profile,
 		'text':this.text,
 		'slug':this.slug,
