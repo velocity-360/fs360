cdf54d282eddafad6421dfe5def61cbf9eca7cb2
fullstack360 <info@fullstack360.com>
2016-08-08 15:13:19 -0400

    survey property on subscriber

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -1,24 +1,23 @@
-var mongoose = require('mongoose');
-
+var mongoose = require('mongoose')
 
 var SubscriberSchema = new mongoose.Schema({
 	name: {type:String, trim:true, default:''},
 	email: {type:String, trim:true, default:''},
+	survey: {type:Array, default:[]},
 	workshop: {type:String, trim:true, lowercase:true, default:''},
 	timestamp: {type:Date, default:Date.now},
-});
-
+})
 
 SubscriberSchema.methods.summary = function() {
 	var summary = {
 		'name':this.name,
 		'email':this.email,
+		'survey':this.survey,
 		'workshop':this.workshop,
 		'timestamp':this.timestamp,
 		'id':this._id
-	};
-	return summary;
-};
+	}
+	return summary
+}
 
-
-module.exports = mongoose.model('SubscriberSchema', SubscriberSchema);
+module.exports = mongoose.model('SubscriberSchema', SubscriberSchema)
