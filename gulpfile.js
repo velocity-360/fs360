f352c66fb573a3dacc05e5c6e5cedde3d730e9eb
fullstack360 <info@fullstack360.com>
2016-08-21 06:20:47 -0400

    minify css gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 // including plugins
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify'),
     to5 = require('gulp-6to5');
+
 
 gulp.task('es6-es5', function(){
 	return gulp.src([
@@ -15,6 +18,27 @@ gulp.task('es6-es5', function(){
 		)
 		.pipe(to5())
 		.pipe(gulp.dest('./public/build/es5/'));
+});
+
+gulp.task('css', function(){
+    return gulp.src(
+            [
+                './public/css/custom.css',
+                './public/css/bootstrap.css',
+                './public/css/style.css',
+                './public/css/dark.css',
+                './public/css/font-icons.css',
+                './public/css/animate.css',
+                './public/css/magnific-popup.css',
+                './public/css/font-awesome.min.css',
+                './public/css/components/pricing-table.css',
+                './public/css/responsive.css'
+            ]
+        )
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gp_concat('style.min.css'))
+        .pipe(gulp.dest('./public/build/css/'))
 });
 
 gulp.task('build', function(){
@@ -37,4 +61,4 @@ gulp.task('watch', function() {
     gulp.watch(['./public/app/ServerApp.js', './public/app/*/**.js', './public/app/*/*/**.js'], ['es6-es5']);
 });
 
-gulp.task('default', ['es6-es5', 'build', 'watch'], function(){});
+gulp.task('default', ['es6-es5', 'build', 'css', 'watch'], function(){});
--- public/js/tracker.js
+++
@@ -1,3 +0,0 @@
-trackVisit = function(){
-	
-}
