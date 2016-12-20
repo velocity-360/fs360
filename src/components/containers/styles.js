d03b3c943c6c36394b88fe49c946df39ab5fbe35
fullstack360 <info@fullstack360.com>
2016-12-20 08:26:11 -0500

    loader style

--- src/components/containers/styles.js
+++ src/components/containers/styles.js
@@ -134,6 +134,23 @@ export default {
 	},
 	paragraph: {
 		fontWeight: 200
+	},
+	loader: {
+	    lines: 13,
+	    length: 20,
+	    width: 10,
+	    radius: 30,
+	    corners: 1,
+	    rotate: 0,
+	    direction: 1,
+	    color: '#fff',
+	    speed: 1,
+	    trail: 60,
+	    shadow: false,
+	    hwaccel: false,
+	    zIndex: 2e9,
+	    top: '50%',
+	    left: '50%',
+	    scale: 1.00
 	}
-
 }
