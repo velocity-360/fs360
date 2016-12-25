5898ca58756116843293691d5cfd2d379cdf5acb
fullstack360 <info@fullstack360.com>
2016-12-24 20:26:39 -0500

    misc

--- src/components/containers/FeaturedTutorials.js
+++ src/components/containers/FeaturedTutorials.js
@@ -24,11 +24,11 @@ class FeaturedTutorials extends Component {
 							    <div className="caption">
 	                                <img style={styles.icon} src={'https://media-service.appspot.com/site/images/'+tutorial.image+'?crop=320'} />
 									<h3 style={styles.title}>
-										<Link to={'/tutorial/'+tutorial.slug}>{ TextUtils.truncateText(tutorial.title, 19) }</Link>
+										<Link to={'/tutorial/'+tutorial.slug}>{ TextUtils.truncateText(tutorial.title, 22) }</Link>
 									</h3>
 									<hr />
 									<p style={styles.paragraph}>
-										{ TextUtils.truncateText(tutorial.description, 190) }
+										{ TextUtils.truncateText(tutorial.description, 180) }
 									</p>
 									<div style={{textAlign:'right'}}>
 										<Link to={'/tutorial/'+tutorial.slug} className="btn btn-primary" role="button">Read More</Link>
