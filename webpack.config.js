346308921fbee0fcf7b36020805b51927c33a0da
fullstack360 <info@fullstack360.com>
2016-12-25 10:09:40 -0500

    misc

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,15 @@ var webpack = require("webpack");
 var path = require('path');
 
 module.exports = {
-	// entry: {
-	// 	app: "./public/app/App.js"
-	// },
-	// output: {
-	// 	filename:"public/build/bundle.js",
- //        sourceMapFilename: "public/build/bundle.map"
-	// },
+
 	entry: {
 		app: './src/app.js'
 	},
 	output: {
 		filename: 'public/dist/bundle.js',
         sourceMapFilename: 'public/dist/bundle.map'
-	},    devtool: '#source-map',	
+	},
+	devtool: '#source-map',	
 	plugins: process.env.NODE_ENV === 'production' ? [
 	    new webpack.DefinePlugin({
 	        'process.env': {
