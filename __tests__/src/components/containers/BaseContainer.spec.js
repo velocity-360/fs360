d4855362ee9a070775cea2818912c3707456e3cb
fullstack360 <info@fullstack360.com>
2017-02-26 14:11:39 -0500

    test file for base container

---
+++ __tests__/src/components/containers/BaseContainer.spec.js
@@ -0,0 +1,10 @@
+import { BaseContainer } from '../../../../src/components/containers'
+
+describe('Base Container', () => {
+
+	it ('subscriber has email and name', () => {
+		expect(0).toEqual(0)
+
+	})
+
+})
