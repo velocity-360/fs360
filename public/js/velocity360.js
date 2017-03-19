bc9299b5dcdec93f4ce6074670471deba61f1fe9
fullstack360 <info@fullstack360.com>
2017-03-19 06:05:34 -0400

    register, login functinos

--- public/js/velocity360.js
+++ public/js/velocity360.js
@@ -1,1 +1,22 @@
-console.log('HELLO!')
+var visitor = {
+	username: '',
+	email: '',
+	password: ''
+}
+
+var updateVisitor = function(event){
+	event.preventDefault()
+	visitor[event.target.id] = event.target.value
+}
+
+var login = function(event){
+	event.preventDefault()
+	console.log('LOGIN: '+JSON.stringify(visitor))
+
+}
+
+var register = function(event, type){ // basic or premium
+	event.preventDefault()
+	console.log('REGISTER: '+JSON.stringify(visitor)+', '+type)
+
+}
