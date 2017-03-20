927e879fcb207327f758ba17d796564061361d12
fullstack360 <info@fullstack360.com>
2017-03-20 09:19:34 -0400

    removed theme from dash directory

--- gulpfile.js
+++ gulpfile.js
@@ -74,6 +74,42 @@ gulp.task('build', function(){
         .pipe(gulp.dest('./public/dist/'))
 });
 
+gulp.task('dashboard-css', function(){
+    return gulp.src(
+            [            
+                './public/css/base.css',
+                './public/css/DateTimePicker.css',
+                './public/css/owl.carousel.css',
+                './public/css/owl.theme.default.css'
+            ]
+        )
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gp_concat('style.min.css'))
+        .pipe(gulp.dest('./public/dist/dash/css/'))
+})
+
+gulp.task('dashboard-build', function(){
+    return gulp.src(
+            [
+                './public/js/jquery-1.11.2.min.js',
+                './public/js/common_scripts_min.js',
+                './public/js/functions.js',
+                './public/js/DateTimePicker.js',
+                './public/js/theia-sticky-sidebar.js',
+                './public/js/sidebar-config.js',
+                './public/js/sweetalert.min.js',
+                './public/js/velocity360.js',
+                './public/js/tutorials.js'
+            ]
+        )
+        .pipe(gp_concat('gulp-concat.js'))
+        .pipe(gulp.dest('./public/min/'))
+        .pipe(gp_rename('vendor.min.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('./public/dist/dash/js/'))
+});
+
 gulp.task('watch', function() {
     gulp.watch(['./src/serverapp.js', './src/*/**.js', './src/*/*/**.js', './public/js/**.js'], ['es6-es5', 'build'])
 })
