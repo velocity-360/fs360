9e629d3a8d927df6fb233ca4899acdf2933052b7
fullstack360 <info@fullstack360.com>
2017-03-28 05:29:56 -0400

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 			{
 				test: /\.jsx?$/,
 				exclude: /(node_modules|bower_components)/,
-				loader: 'babel',
+				loader: 'babel-loader',
 				query:{
 					presets:['react', 'es2015']
 				}
